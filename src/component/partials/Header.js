003fbf230f3df72ac935233da613bf9ce87534b4
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-19 23:31:45 -0400

    Logo resized

--- src/component/partials/Header.js
+++ src/component/partials/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
     <header>
         <nav className="flex items-center flex-wrap justify-around text-white p-6 bg-slate-800">
             <NavLink className="inline-block px-10 py-3 text-center border-violet-600 bg-violet-600 rounded-lg duration-200  hover:bg-violet-700 hover:border-violet-800"to="/">Dashboard</NavLink>
-            <img className="w-2/5" src={Logo} alt="Logo"></img>
+            <img className="w-1/4" src={Logo} alt="Logo"></img>
             <NavLink className="inline-block px-10 py-3 text-center border-violet-600 bg-violet-600 rounded-lg duration-200  hover:bg-violet-700 hover:border-violet-800"to="/addExpense">Add Expense</NavLink>
         </nav>
     </header>
