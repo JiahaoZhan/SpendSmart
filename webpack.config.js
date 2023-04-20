12b90c0f1dd71a3c9bcb7eeb96e6fb47581f70fe
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-19 23:10:33 -0400

    added logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
 
-module.exports = {
-  mode: "development",
-  entry: './src/App.js',
+module.exports = (env) => {
+  const isProduction = env == 'production';
+  
+  return {
+  entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        type: "asset/resource",
+      },
       {
       loader: 'babel-loader',
       test: /\.js$/,
@@ -21,12 +27,14 @@ module.exports = {
         'css-loader',
         'sass-loader',
         'postcss-loader'
-      ]
-    },]
+      ],
+    },
+    ]
   },
-  devtool: 'eval',
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
   devServer: {
     static: path.join(__dirname, 'public'),
     historyApiFallback: true,
+    
   }
-};
+}};
