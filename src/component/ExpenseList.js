44a152ff9ab19ce1bdd1eb27294f2fdd5a8cad18
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-19 23:47:49 -0400

    Improve responsiveness on different screen size

--- src/component/ExpenseList.js
+++ src/component/ExpenseList.js
@@ -6,7 +6,7 @@ import getFilteredExpenses from "../selectors/expenses"
 const ExpenseList = () => {
     const expenses = useSelector((state) => getFilteredExpenses(state.expenses, state.filters))
     return (
-    <div className="flex flex-wrap justify-center flex-col md:flex-row">
+    <div className="flex flex-wrap justify-center items-center flex-col md:flex-row">
         {
             expenses.map((expense)=>(
                 <Expense key={expense.id} {...expense}/>
--- src/component/partials/Header.js
+++ src/component/partials/Header.js
@@ -5,9 +5,9 @@ import Logo from "../../tailwind/logo.svg"
 const Header = () => (
     <>
     <header>
-        <nav className="flex items-center flex-wrap justify-around text-white p-6 bg-slate-800">
+        <nav className="grid md:grid-cols-3 grid-cols-1 items-center text-white p-6 bg-slate-800">
             <NavLink className="inline-block px-10 py-3 text-center border-violet-600 bg-violet-600 rounded-lg duration-200  hover:bg-violet-700 hover:border-violet-800"to="/">Dashboard</NavLink>
-            <img className="w-1/4" src={Logo} alt="Logo"></img>
+            <img  src={Logo} alt="Logo"></img>
             <NavLink className="inline-block px-10 py-3 text-center border-violet-600 bg-violet-600 rounded-lg duration-200  hover:bg-violet-700 hover:border-violet-800"to="/addExpense">Add Expense</NavLink>
         </nav>
     </header>
